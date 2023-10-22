e00ad41dbcd33e397ce9b0a27e1f8b8925f23822
Spitfire1970 <zcabgoy@ucl.ac.uk>
2023-10-22 01:44:41 +0100

    added README and removed tests

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2 as cv
-import sys
-import tensorflow as tf
-img = cv.imread("Screenshot 2023-08-18 at 3.28.38 AM.png")
-img = cv.resize(img, (30, 30))
-print(img.shape)
-model = tf.keras.models.load_model("model1.h5")
-print(model.predict(np.array(img).reshape(1,30,30,3)).argmax())
--- test2.py
+++
@@ -1,3 +0,0 @@
-import os
-import cv2 as cv
-print()
